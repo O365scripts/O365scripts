00bb29525b94473b7911df6b35f36489c8d029c3
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-05-03 19:39:08 -0400

    Update O365 - Connect to Microsoft Teams.ps1

--- O365 - Connect to Microsoft Teams.ps1
+++ O365 - Connect to Microsoft Teams.ps1
@@ -1,32 +1,34 @@
 <#
 .SYNOPSIS
-    Connecto to Microsoft Teams PowerShell
+	Connect to Microsoft Teams PowerShell
 .NOTES
-    You will need to specify the TeamsEnvironmentName when connecting to a GCC High or a DOD tenant (TeamsGCCH or TeamsDOD).
-    Use the TenantId attribute when connecting to a different tenant.
-    Use the LogFilePath and LogLevel when reporting bugs.
+ > You will need to specify the -TeamsEnvironmentName when connecting to a GCC High or a DOD tenant (TeamsGCCH or TeamsDOD).
+ > Use the TenantId attribute when connecting to a different tenant.
+ > Use the -LogFilePath and -LogLevel when reporting bugs.
 .LINK
-    https://www.powershellgallery.com/packages/MicrosoftTeams/
-    https://docs.microsoft.com/en-us/powershell/module/teams/connect-microsoftteams?view=teams-ps
+https://www.powershellgallery.com/packages/MicrosoftTeams/
+https://docs.microsoft.com/en-us/powershell/module/teams/connect-microsoftteams?view=teams-ps
 #>
 
-<#  Install module as administrator? #>
-Install-Module -Name MicrosoftTeams -Force;
+<# Install the Teams module as administrator? #>
+Install-Module MSOnline -Force -Confirm:$false;
+<# Install the Teams module as user? #>
+Install-Module MSOnline -Scope CurrentUser -Force -Confirm:$false;
+<# Force reinstallation of the Teams module? #>
+Remove-Module MicrosoftTeams -Force -Confirm:$false -ErrorAction SilentlyContinue; Install-Module MicrosofTeams -Force -Confirm:$false;
 
-<#  Install module as user? #>
-Install-Module -Name MicrosoftTeams -Scope CurrentUser -Force;
+<# Connect to Teams? #>
+$me = "admin@domain.onmicrosoft.com";
+Connect-MicrosoftTeams -AccountId $me;
 
-<# Connect to Teams #>
-    $me = "admin@domain.onmicrosoft.com";
-Connect-MicrosoftTeams -AccountId $me
-
-<# Connect to Teams without MFA. #>
-    $me = "admin@domain.onmicrosoft.com";
+<# Connect to Teams without using MFA but cache credentials? #>
+$me = "admin@domain.onmicrosoft.com";
 $creds = Get-Credential -Message "Login:" -UserName $me;
 Connect-MicrosoftTeams -AccountId $me -Credential $creds;
+<#$creds = $null;#>
 
-<# Connect to Teams without MFA but do not store credentials. #>
-    $me = "user@domain.com";
+<# Connect to Teams without using MFA and without caching credentials? #>
+$me = "admin@domain.onmicrosoft.com";
 Connect-MicrosoftTeams -AccountId $me -Credential (Get-Credential -Message "Login:" -UserName $me);
 
 <# Disconnect session? #>
