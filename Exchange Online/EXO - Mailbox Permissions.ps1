0c5dbf0c41f6d060534030d69d7b263758d772c6
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-06-24 15:26:06 -0400

    Create EXO - Mailbox Permissions.ps1

---
+++ Exchange Online/EXO - Mailbox Permissions.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+Mailbox Permissions Management
+.NOTES
+
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/exchange/mailboxes/add-mailboxpermission?view=exchange-ps
+https://docs.microsoft.com/en-us/powershell/module/exchange/mailboxes/remove-mailboxpermission?view=exchange-ps
+https://docs.microsoft.com/en-us/outlook/troubleshoot/domain-management/remove-automapping-for-shared-mailbox
+#>
+
+<# Get list of mailboxes and assign full access permissions to an account or a group. #>
+$grant_to = "admins@domain.com";
+
+$mbox = Get-Mailbox -ResultSize Unlimited;
+#$mbox = Get-Mailbox -ResultSize Unlimited | Where {$_.RecipientTypeDetails -eq "UserMailbox"};
+#$mbox = Get-Mailbox -ResultSize Unlimited | Where {$_.RecipientTypeDetails -eq "SharedMailbox"};
+#$mbox = Get-Mailbox -ResultSize Unlimited | Where {$_.RecipientTypeDetails -eq "EquipmentMailbox"};
+#$mbox = Get-Mailbox -ResultSize Unlimited | Where {$_.RecipientTypeDetails -eq "RoomMailbox"};
+
+Foreach ($m in $mbox) {
+	Write-Host -NoNewline "Trying to grant Full Access permissions on ";
+		Write-Host -NoNewline -Fore Yellow $m.PrimarySmtpAddress;
+		Write-Host -NoNewline " to ";
+		Write-Host -NoNewline -Fore Yellow $grant_to;
+		Write-Host -NoNewline ".";
+	Add-MailboxPermission -Identity $m.PrimarySmtpAddress -User $grant_to -AccessRights FullAccess -AutoMapping:$false -ErrorAction Continue;
+	}
