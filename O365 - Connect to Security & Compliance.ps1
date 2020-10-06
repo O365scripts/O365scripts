97bb1eb935248d90b7c9f3472f2b2f4b12749501
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-10-06 15:48:34 -0400

    Create O365 - Connect to Security & Compliance.ps1

---
+++ O365 - Connect to Security & Compliance.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+Connect to Security & Compliance.
+.NOTES
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/exchange/connect-ippssession?view=exchange-ps
+#>
+
+<# QUICKRUN: Install and Connect to S&C via the EXO v2 module. #>
+#Set-ExecutionPolicy RemoteSigned;
+$Me = "admin@mytenant.onmcirosoft.com";
+#Install-Module ExchangeOnlineManagement -Confirm:$false -Force;
+Import-Module ExchangeOnlineManagement;
+Connect-IPPSSession -UserPrincipalName $Me;
+
+<# Connect to Security & Compliance via the EXO v2 module. #>
+$Me = "";
+Connect-IPPSSession -UserPrincipalName $Me;
+
+<# Connect to Security & Compliance using stored creds. #>
+$Me = "";
+$Creds = Get-Credential -UserName $Me -Message "Login";
+Connect-IPPSSession -UserPrincipalName $Me -Credential $Creds;
+
+<# Connect to Security & Compliance without storing creds. #>
+$Me = "";
+Connect-IPPSSession -UserPrincipalName $Me -Credential (Get-Credential -UserName $Me -Message "Login");
