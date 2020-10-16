5badb3a4cfe6be9d67efa54b2813d14aa6b2cadc
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-10-16 10:15:32 -0400

    Update EXO - Distribution Group Member Management.ps1

--- Exchange Online/EXO - Distribution Group Member Management.ps1
+++ Exchange Online/EXO - Distribution Group Member Management.ps1
@@ -1,5 +1,16 @@
 <#
+.SYNOPSIS
+
+.NOTES
+
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/exchange/add-distributiongroupmember
+https://support.microsoft.com/en-us/office/distribution-groups-e8ba58a8-fab2-4aaf-8aa1-2a304052d2de
 #>
+
+<# Connect to EXO v2. #>
+$Me = "";
+Connect-ExchangeOnline -UserPrincipalName $Me;
 
 <# Interactive selection of one or multiple users to be added to one or multiple distribution groups. #>
 $ListDG = Get-DistributionGroup -RecipientTypeDetails "MailUniversalDistributionGroup" -ResultSize Unlimited | Out-GridView -PassThru;
