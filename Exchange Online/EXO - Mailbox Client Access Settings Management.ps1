c93813bc49805c99bdb87fc1b283a34212ea9c4b
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-10-20 14:09:16 -0400

    Create EXO - Mailbox Client Access Settings Management.ps1

---
+++ Exchange Online/EXO - Mailbox Client Access Settings Management.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+.NOTES
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/exchange/client-access/set-casmailbox?view=exchange-ps
+https://docs.microsoft.com/en-us/powershell/module/exchange/get-distributiongroupmember?view=exchange-ps
+https://docs.microsoft.com/en-us/powershell/module/exchange/get-distributiongroup?view=exchange-ps
+#>
+
+<# Interactive selection of one or multiple security groups and pass through each of their members to bulk adjust disable EAS, POP and IMAP. #>
+$eas = $false; $imap = $false; $pop = $false;
+Get-DistributionGroup -RecipientTypeDetails MailUniversalSecurityGroup | Out-GridView -PassThru | % {Get-DistributionGroupMember -Identity $_.DistinguishedName | % {Set-CasMailbox -Identity $_.DistinguishedName -ActiveSyncEnabled:$eas -ImapEnabled:$imap -PopEnabled:$pop;}}
