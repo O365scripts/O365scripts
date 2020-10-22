057ba6556d186dbda0805f4d5c3974f02a7057ed
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-10-22 08:30:40 -0400

    Update EXO - Trusted or Blocked Senders Mailbox Management.ps1

--- Exchange Online/EXO - Trusted or Blocked Senders Mailbox Management.ps1
+++ Exchange Online/EXO - Trusted or Blocked Senders Mailbox Management.ps1
@@ -1,4 +1,7 @@
 <#
+.SYNOPSIS
+Trusted/Blocked Sender Mailbox Junk Email Configuration Management
+.NOTES
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/exchange/get-mailboxjunkemailconfiguration?view=exchange-ps
 https://docs.microsoft.com/en-us/powershell/module/exchange/set-mailboxjunkemailconfiguration?view=exchange-ps
@@ -9,3 +12,9 @@ $TrustedSender = "user@domain.com"
 $ListMailbox = Get-Mailbox -ResultSize Unlimited | Select DisplayName,PrimarySmtpAddress,RecipientTypeDetails,DistinguishedName,@{Name="TrustedSendersAndDomains";Expression={(Get-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -ErrorAction SilentlyContinue).TrustedSendersAndDomains}};
 $ListMailbox | Where {$_.TrustedSendersAndDomains -notcontains $TrustedSender} | % {Set-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -TrustedSendersAndDomains @{add=$TrustedSender}};
 #$ListMailbox | Where {$_.TrustedSendersAndDomains -contains $TrustedSender} | % {Set-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -TrustedSendersAndDomains @{remove=$TrustedSender}};
+
+<# Add/remove a blocked sender on all mailboxes that do not already contain it. #>
+$BlockedSender = "user@domain.com"
+$ListMailbox = Get-Mailbox -ResultSize Unlimited | Select DisplayName,PrimarySmtpAddress,RecipientTypeDetails,DistinguishedName,@{Name="BlockedSendersAndDomains";Expression={(Get-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -ErrorAction SilentlyContinue).BlockedSendersAndDomains}};
+$ListMailbox | Where {$_.BlockedSendersAndDomains -notcontains $BlockedSender} | % {Set-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -BlockedSendersAndDomains @{add=$BlockedSender}};
+#$ListMailbox | Where {$_.BlockedSendersAndDomains -contains $BlockedSender} | % {Set-MailboxJunkEmailConfiguration -Identity $_.DistinguishedName -BlockedSendersAndDomains @{remove=$BlockedSender}};
