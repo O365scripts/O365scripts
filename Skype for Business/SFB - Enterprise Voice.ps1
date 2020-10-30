c9936fc60158c94e96d0a5849f10b4dbb007bc52
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-10-30 18:04:39 -0400

    Update SFB - Enterprise Voice.ps1

--- Skype for Business/SFB - Enterprise Voice.ps1
+++ Skype for Business/SFB - Enterprise Voice.ps1
@@ -3,6 +3,13 @@
 .LINK
 #>
 
+<# QUICKRUN: Install the Teams module and connect to SFBO. #>
+$Me = "admin@mytenant.onmicrosoft.com";
+$Tenant = "mytenant";
+Install-Module MicrosoftTeams -Force -Confirm:$false;
+$Session_Sfb = New-CsOnlineSession -OverrideAdminDomain "$Tenant.onmicrosoft.com";
+Import-PSSession $Session_Sfb -AllowClobber;
+
 <# Toggle Enterprise Voice on a specific user. #>
 $User = "user@domain.com";
 Set-CsUser -Identity $User -EnterpriseVoiceEnabled $false;
