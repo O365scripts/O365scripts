b491629b131b3b7498f1c41ef19d165e8d8df05b
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-04 10:09:55 -0500

    Update Get-M365DomainDnsOverview.ps1

--- Tools/Get-M365DomainDnsOverview.ps1
+++ Tools/Get-M365DomainDnsOverview.ps1
@@ -1,10 +1,9 @@
 <#
 .SYNOPSIS
-
 .NOTES
-
 .LINK
-
+https://docs.microsoft.com/en-us/powershell/module/dnsclient/resolve-dnsname?view=win10-ps
+https://docs.microsoft.com/en-us/microsoft-365/admin/get-help-with-domains/create-dns-records-at-any-dns-hosting-provider?view=o365-worldwide
 .EXAMPLE
 Get-M365DomainDnsOverview;
 Get-M365DomainDnsOverview "microsoft.com";
