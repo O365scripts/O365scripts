8d0ac6e58d75043c12ba19b7be618421bd6b580b
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-04 15:55:25 -0500

    Update SFB - Skype User Overview.ps1

--- Skype for Business/SFB - Skype User Overview.ps1
+++ Skype for Business/SFB - Skype User Overview.ps1
@@ -7,9 +7,8 @@
 <# Skype/Teams User Overview. #>
 $User = "user@domain.com";
 $Path_CsvOut = "$env:USERPROFILE\Desktop"; 
-$UserOut = $User.Replace("@","_");
-Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Export-Csv -Path "$Path_CsvOut\Get-CsOnlineUser__$UserOut_$Stamp.csv" -Encoding utf8 -NoTypeInformation;
-Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Export-Csv -Path "$Path_CsvOut\Get-CsOnlineVoiceUser__$UserOut_$Stamp.csv" -Encoding utf8 -NoTypeInformation;
-Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Export-Csv -Path "$Path_CsvOut\CsOnlineDialInConferencingUser__$UserOut_$Stamp.csv" -Encoding utf8 -NoTypeInformation;
-Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Export-Csv -Path "$Path_CsvOut\CsOnlineApplicationInstance__$UserOut_$Stamp.csv" -Encoding utf8 -NoTypeInformation;
-Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Export-Csv -Path "$Path_CsvOut\Get-CsUser__$UserOut_$Stamp.csv" -Encoding utf8 -NoTypeInformation;
+Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineUser__$($User.Replace("@","_"))_$Stamp.csv";
+Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineVoiceUser__$($User.Replace("@","_"))_$Stamp.csv";
+Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\CsOnlineDialInConferencingUser__$($User.Replace("@","_"))_$Stamp.csv";
+Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\CsOnlineApplicationInstance__$($User.Replace("@","_"))_$Stamp.csv";
+Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsUser__$($User.Replace("@","_"))_$Stamp.csv";
