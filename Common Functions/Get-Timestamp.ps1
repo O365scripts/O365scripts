2dea3dbb53d111aae95092e7534af7e7d8525674
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-03 23:35:49 -0500

    Update Get-Timestamp.ps1

--- Common Functions/Get-Timestamp.ps1
+++ Common Functions/Get-Timestamp.ps1
@@ -1,10 +1,12 @@
 <#
 .SYNOPSIS
-Function to generate a full timestamp.
+Simple function to generate a full timestamp.
 .NOTES
+Version Update: 2020-11-03
+Author: https://github.com/O365scripts
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7
 #>
 
 <# Timestamper. #>
-function Get-Timestamp {$Stamp = Get-Date -Format "yyyyMMddHHmmss"; $Stamp;}
+function Get-Timestamp {Get-Date -Format "yyyyMMddHHmmss"}
