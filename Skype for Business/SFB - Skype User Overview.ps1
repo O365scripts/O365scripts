3ebd13e67adc0358452c9ee5ecaabe8b2182f11d
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-06 15:48:43 -0500

    Update SFB - Skype User Overview.ps1

--- Skype for Business/SFB - Skype User Overview.ps1
+++ Skype for Business/SFB - Skype User Overview.ps1
@@ -5,7 +5,6 @@
 #>
 
 <# QUICKRUN: Install the Teams module and connect to SFBO. #>
-$Me = "admin@mytenant.onmicrosoft.com";
 $Tenant = "mytenant";
 Install-Module MicrosoftTeams -Force -Confirm:$false;
 $Session_Sfb = New-CsOnlineSession -OverrideAdminDomain "$Tenant.onmicrosoft.com";
@@ -18,7 +17,7 @@ $FormatEnumerationLimit = -1;
 $User = "user@domain.com";
 $Stamp = Get-Date -Format "yyyyMMddHHmmss";
 Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineUser__$($User.Replace("@","_"))_$Stamp.txt";
-Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineVoiceUser__$($User.Replace("@","_"))_$Stamp.csv";
-Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineDialInConferencingUser__$($User.Replace("@","_"))_$Stamp.csv";
-Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineApplicationInstance__$($User.Replace("@","_"))_$Stamp.csv";
+#Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineVoiceUser__$($User.Replace("@","_"))_$Stamp.csv";
+#Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineDialInConferencingUser__$($User.Replace("@","_"))_$Stamp.csv";
+#Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineApplicationInstance__$($User.Replace("@","_"))_$Stamp.csv";
 #Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsUser__$($User.Replace("@","_"))_$Stamp.txt";
