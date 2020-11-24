0e8a62430986a1728376e73c8ec6f904e14f29ac
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-24 14:55:35 -0500

    Update SFB - Audio Conferencing User Management.ps1

--- Skype for Business/SFB - Audio Conferencing User Management.ps1
+++ Skype for Business/SFB - Audio Conferencing User Management.ps1
@@ -1,4 +1,10 @@
 <#
+.SYNOPSIS
+.NOTES
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinedialinconferencinguser?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/set-csonlinedialinconferencinguser?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineuser?view=skype-ps
 #>
 
 
@@ -10,6 +16,5 @@ Enable-CsOnlineDialInConferencingUser $User;
 <# Export CsOnlineUser and ConferencingUser details of a specific user. #>
 $User = "user@domain.com";
 $UserOut = $User.Replace("@","_");
-$Path_CsvOut = "$env:USERPROFILE\Desktop";
-Get-CsOnlineUser -Identity $User | Export-Csv -Path "$Path_CsvOut\Get-CsOnlineUser - $UserOut.csv" -Encoding utf8 -NoTypeInformation;
-Get-CsOnlineDialInConferencingUser -Identity $User | Export-Csv -Path "$Path_CsvOut\CsOnlineDialInConferencingUser - $UserOut.csv" -Encoding utf8 -NoTypeInformation;
+Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineUser__$($User.Replace("@","_"))_$Stamp.txt";
+Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineDialInConferencingUser__$($User.Replace("@","_"))_$Stamp.txt";
