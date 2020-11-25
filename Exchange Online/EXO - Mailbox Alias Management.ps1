7832db2b42277be4d67cdb0c026e4a8c5c855764
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-25 15:29:33 -0500

    Update EXO - Mailbox Alias Management.ps1

--- Exchange Online/EXO - Mailbox Alias Management.ps1
+++ Exchange Online/EXO - Mailbox Alias Management.ps1
@@ -1,13 +1,24 @@
 <#
 .SYNOPSIS
-	Mailbox Alias Management
+Mailbox Alias Management
 .NOTES
 .LINK
 https://docs.microsoft.com/en-us/exchange/recipients/user-mailboxes/email-addresses?view=exchserver-2019
+https://docs.microsoft.com/en-us/powershell/module/exchange/set-mailbox?view=exchange-ps
+https://docs.microsoft.com/en-us/powershell/module/exchange/get-mailbox?view=exchange-ps
 #>
 
-<# Add or remove an alias to a specific mailbox.#>
+<# Add or remove an alias on a specific mailbox.#>
 $mbox = "user@domain.com";
 $alias = "user2@domain.com";
 Set-Mailbox $mbox -EmailAddresses @{add=$alias};
 Set-Mailbox $mbox -EmailAddresses @{remove=$alias};
+
+<#####################>
+
+<# Add multiple aliases from a CSV into a specified mailbox. #>
+$User = "user@domain.com";$PathCsv = "$env:USERPROFILE\Desktop\BulkAddAliasesToMailbox.csv";
+Import-Csv $PathCsv | % {Set-Mailbox $User -EmailAddresses @{add=$_.SmtpAddress}};
+
+<# Verify aliases. #>
+(Get-Mailbox $User).EmailAddresses
