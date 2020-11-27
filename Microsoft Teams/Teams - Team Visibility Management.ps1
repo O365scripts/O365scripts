eadac8fbeb583c754ba9fe276317df1ae6cda90b
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-11-26 20:47:39 -0500

    Update Teams - Team Visibility Management.ps1

--- Microsoft Teams/Teams - Team Visibility Management.ps1
+++ Microsoft Teams/Teams - Team Visibility Management.ps1
@@ -1,8 +1,10 @@
 <#
 .SYNOPSIS
+Teams Visibility Management
 .NOTES
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/teams/set-team?view=teams-ps
+https://support.microsoft.com/en-us/office/make-a-public-team-private-in-teams-6f324fbc-6599-4612-8daa-ff5d35a746bf
 #>
 
 <# Set the visibility of a Team to Public/Private. #>
