dc2837b3c673605f6c84c08640b9eabcba844f42
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-12-22 13:41:20 -0500

    Update EXO - Tenant Hydration.ps1

--- Exchange Online/EXO - Tenant Hydration.ps1
+++ Exchange Online/EXO - Tenant Hydration.ps1
@@ -2,6 +2,7 @@
 .NOTES
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/exchange/enable-organizationcustomization?view=exchange-ps
+https://docs.microsoft.com/en-us/powershell/module/exchange/get-organizationconfig?view=exchange-ps
 #>
 
 <# QUICKRUN: Install and Connect to EXO v2. #>
@@ -13,3 +14,6 @@ Connect-ExchangeOnline -UserPrincipalName $me;
 
 <# Force hydration of the tenant. #>
 Enable-OrganizationCustomization
+
+<# Confirm? #>
+Get-OrganizationConfig | fl Name,isDehydrated
