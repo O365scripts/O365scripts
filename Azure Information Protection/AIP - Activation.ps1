9476475f84d6b623affd1e5f063283c3774dcb2d
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2020-12-23 18:56:01 -0500

    Create AIP - Activation.ps1

---
+++ Azure Information Protection/AIP - Activation.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+.NOTES
+.LINK
+https://docs.microsoft.com/en-us/azure/information-protection/install-powershell
+https://docs.microsoft.com/en-us/azure/information-protection/activate-service
+#>
+
+<# #>
