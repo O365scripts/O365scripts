0a89ccd62751e1ead98433514d9de3b2c9d1901f
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2021-01-17 21:59:00 -0500

    Update Get-Timestamp.ps1

--- Common Functions/Get-Timestamp.ps1
+++ Common Functions/Get-Timestamp.ps1
@@ -1,11 +1,17 @@
 <#
 .SYNOPSIS
 Simple function to generate a full timestamp.
+
 .NOTES
-Version Update: 2020-11-03
+Version Update: 2020-12-13
 Author: https://github.com/O365scripts
+
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7
+
+.EXAMPLE
+$Report | Export-Csv -Encoding utf8 -Path "Report_$(Get-Timestamp).csv";
+$Report | Out-File -Encoding utf8 -FilePath "Report_$(Get-Timestamp).txt";
 #>
 
 <# Timestamper. #>
