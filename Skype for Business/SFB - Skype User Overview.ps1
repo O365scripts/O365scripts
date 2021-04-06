688933e3e2a3956fac7e79b5168cbd0be2354317
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2021-04-06 10:33:10 -0400

    Update SFB - Skype User Overview.ps1

--- Skype for Business/SFB - Skype User Overview.ps1
+++ Skype for Business/SFB - Skype User Overview.ps1
@@ -1,16 +1,22 @@
 <#
 .SYNOPSIS
+Get Skype User Overview Details
+https://github.com/O365scripts/O365scripts/blob/master/Skype%20for%20Business/SFB%20-%20Skype%20User%20Overview.ps1
 .NOTES
 .LINK
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineuser?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinevoiceuser?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinedialinconferencinguser?view=skype-ps
 #>
 
-<# QUICKRUN: Install the Teams module and connect to SFBO. #>
-$Tenant = "mytenant";
+
+<# Install the previous Teams module version and connect to SFBO using CsOnlineSession instead. #>
 #Set-ExecutionPolicy RemoteSigned;
-#Install-Module MicrosoftTeams -Force -Confirm:$false;
-Import-Module MicrosoftTeams;
-$Session_Sfb = New-CsOnlineSession -OverrideAdminDomain "$Tenant.onmicrosoft.com";
+#Install-Module MicrosoftTeams -RequiredVersion 1.1.6 -Force -Confirm:$false;
+Import-Module MicrosoftTeams -RequiredVersion 1.1.6;
+$Session_Sfb = New-CsOnlineSession -OverrideAdminDomain "mytenant.onmicrosoft.com";
 Import-PSSession $Session_Sfb;
+
 
 <# Prevent output truncation... #>
 $FormatEnumerationLimit = -1;
@@ -18,8 +24,8 @@ $FormatEnumerationLimit = -1;
 <# Export Skype/Teams User Overview. #>
 $User = "user@domain.com";
 $Stamp = Get-Date -Format "yyyyMMddHHmmss";
-Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineUser__$($User.Replace("@","_"))_$Stamp.txt";
-#Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineVoiceUser__$($User.Replace("@","_"))_$Stamp.txt";
-#Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineDialInConferencingUser__$($User.Replace("@","_"))_$Stamp.txt";
-#Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$Path_CsvOut\Get-CsOnlineApplicationInstance__$($User.Replace("@","_"))_$Stamp.txt";
-#Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsUser__$($User.Replace("@","_"))_$Stamp.txt";
+Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineUser_$Stamp.txt";
+#Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath $env:USERPROFILE\Desktop\Get-CsOnlineVoiceUser_$Stamp.txt";
+#Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineDialInConferencingUser_$Stamp.txt";
+#Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath $env:USERPROFILE\Desktop\Get-CsOnlineApplicationInstance_$Stamp.txt";
+#Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsUser_$Stamp.txt";
