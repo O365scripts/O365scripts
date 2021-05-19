78713dbd861a80014d85b57674c690c6dec7d629
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2021-05-19 09:44:49 -0400

    Update O365 - Connect to Microsoft Teams.ps1

--- O365 - Connect to Microsoft Teams.ps1
+++ O365 - Connect to Microsoft Teams.ps1
@@ -1,7 +1,8 @@
 <#
 .SYNOPSIS
-	Connect to Microsoft Teams PowerShell
+Connect to Microsoft Teams PowerShell
 .NOTES
+ > The recent Teams module versio
  > You will need to specify the -TeamsEnvironmentName when connecting to a GCC High or a DOD tenant (TeamsGCCH or TeamsDOD).
  > Use the TenantId attribute when connecting to a different tenant.
  > Use the -LogFilePath and -LogLevel when reporting bugs.
@@ -10,26 +11,29 @@ https://www.powershellgallery.com/packages/MicrosoftTeams/
 https://docs.microsoft.com/en-us/powershell/module/teams/connect-microsoftteams?view=teams-ps
 #>
 
-<# Install the Teams module as administrator? #>
-Install-Module MicrosoftTeams -Force -Confirm:$false;
-<# Install the Teams module as user? #>
-Install-Module MicrosoftTeams -Scope CurrentUser -Force -Confirm:$false;
-<# Force reinstallation of the Teams module? #>
-Remove-Module MicrosoftTeams -Force -Confirm:$false -ErrorAction SilentlyContinue; Install-Module MicrosofTeams -Force -Confirm:$false;
+<# Connect to Teams. #>
+#Set-ExecutionPolicy RemoteSigned
+#Install-Module MicrosoftTeams -Force -Confirm:$false
+Import-Module MicrosoftTeams;
+Connect-MicrosoftTeams;
 
-<# Connect to Teams? #>
-$me = "admin@domain.onmicrosoft.com";
-Connect-MicrosoftTeams -AccountId $me;
-
-<# Connect to Teams without using MFA but cache credentials? #>
-$me = "admin@domain.onmicrosoft.com";
-$creds = Get-Credential -Message "Login:" -UserName $me;
-Connect-MicrosoftTeams -AccountId $me -Credential $creds;
+<# Connect to Teams without MFA enabled using cached credentials. #>
+$AdminUpn = "admin@domain.com";
+$Creds = Get-Credential -Message "Login:" -UserName $AdminUpn;
+Import-Module MicrosoftTeams;
+Connect-MicrosoftTeams -Credential $Creds;
 <#$creds = $null;#>
 
-<# Connect to Teams without using MFA and without caching credentials? #>
-$me = "admin@domain.onmicrosoft.com";
-Connect-MicrosoftTeams -AccountId $me -Credential (Get-Credential -Message "Login:" -UserName $me);
+<# Connect to Teams without MFA enabled and without caching credentials. #>
+$me = "admin@domain.com";
+Connect-MicrosoftTeams -Credential (Get-Credential -Message "Login:" -UserName $me);
+
+
+<# Confirm Teams module version(s) installed. #>
+Get-Module MicrosoftTeams -ListAvailable | Select Version;
+
+<# Force uninstallation of the Teams module. #>
+#Remove-Module MicrosoftTeams -Force -Confirm:$false -ErrorAction SilentlyContinue;
 
 <# Disconnect session? #>
 Disconnect-MicrosoftTeams;
