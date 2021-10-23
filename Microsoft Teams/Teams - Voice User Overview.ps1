4cff16120db99ab9967338f6105c571cafd8c099
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2021-10-23 19:21:21 -0400

    Update Teams - Voice User Overview.ps1
    Updated document link still pointing to previous SFB folder.
    Updated official reference links
    Updated connection block to use Teams instead of CsOnlineSession.
    
    Added script block to display the overview while another to send all the output to zip.
    Possible changes in the future to pull the

--- Microsoft Teams/Teams - Voice User Overview.ps1
+++ Microsoft Teams/Teams - Voice User Overview.ps1
@@ -1,31 +1,56 @@
 <#
 .SYNOPSIS
-Get Skype User Overview Details
-https://github.com/O365scripts/O365scripts/blob/master/Skype%20for%20Business/SFB%20-%20Skype%20User%20Overview.ps1
+Get Teams Voice User Overview Details
+https://github.com/O365scripts/O365scripts/blob/master/Microsoft%20Teams/Teams%20-%20Voice%20User%20Overview.ps1
 .NOTES
++CsOnlineApplicationInstance/Endpoint should fail normally unless the $User is a Resource Account for some reason.
 .LINK
+Reference:
 https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineuser?view=skype-ps
 https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinevoiceuser?view=skype-ps
 https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinedialinconferencinguser?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineapplicationendpoint?view=skype-ps
+https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineapplicationinstance?view=skype-ps
 #>
 
-
-<# Install the previous Teams module version and connect to SFBO using CsOnlineSession instead. #>
-#Set-ExecutionPolicy RemoteSigned;
-#Install-Module MicrosoftTeams -RequiredVersion 1.1.6 -Force -Confirm:$false;
-Import-Module MicrosoftTeams -RequiredVersion 1.1.6;
-$Session_Sfb = New-CsOnlineSession -OverrideAdminDomain "mytenant.onmicrosoft.com";
-Import-PSSession $Session_Sfb;
-
+<# Connect to Teams. #>
+#Set-ExecutionPolicy RemoteSigned -Force -Confirm:$false;
+#Install-Module MicrosoftTeams -AllowClobber -Force -Confirm:$false;
+Import-Module MicrosoftTeams;
+Connect-MicrosoftTeams;
 
 <# Prevent output truncation... #>
 $FormatEnumerationLimit = -1;
 
-<# Export Skype/Teams User Overview. #>
-$User = "user@domain.com";
-$Stamp = Get-Date -Format "yyyyMMddHHmmss";
-Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineUser_$Stamp.txt";
-#Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath $env:USERPROFILE\Desktop\Get-CsOnlineVoiceUser_$Stamp.txt";
-#Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath "$env:USERPROFILE\Desktop\Get-CsOnlineDialInConferencingUser_$Stamp.txt";
-#Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue | Out-File -FilePath $env:USERPROFILE\Desktop\Get-CsOnlineApplicationInstance_$Stamp.txt";
-#Get-CsUser -Identity $User -ErrorAction SilentlyContinue | Out-File -Encoding utf8 -FilePath "$env:USERPROFILE\Desktop\Get-CsUser_$Stamp.txt";
+
+<# Display Teams Voice User Overview. #>
+$User = "";
+Get-CsOnlineUser -Identity $User | Select *;
+Get-CsOnlineVoiceUser -Identity $User;
+Get-CsOnlineDialInConferencingUser -Identity $User;
+Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue;
+Get-CsOnlineApplicationEndpoint -Uri $User -ErrorAction SilentlyContinue;
+# </TeamsVoiceUserOverview>
+
+
+<# Export Teams Voice User Overview and compress to zip archive. #>
+$User = "";
+$Stamp = Get-Date -Format "yyyyMMddHHmmss"; $PathOut = "$env:TEMP\TeamsVoiceUserOverview_$Stamp";
+$PathExportFile = "$env:USERPROFILE\Desktop\M365-TeamsVoiceUserOverview_$Stamp.zip";
+New-Item $PathOut -ItemType Directory;
+#$CsUser = Get-CsUser -Identity $User -ErrorAction SilentlyContinue;
+#if ($CsUser) {$CsUser | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsUser.txt";}
+$CsOnlineUser = Get-CsOnlineUser -Identity $User -ErrorAction SilentlyContinue;
+if ($CsOnlineUser) {$CsOnlineUser | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsOnlineUser.txt";}
+$CsOnlineVoiceUser = Get-CsOnlineVoiceUser -Identity $User -ErrorAction SilentlyContinue;
+if ($CsOnlineVoiceUser) {$CsOnlineVoiceUser | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsOnlineVoiceUser.txt";}
+$CsOnlineDialInConferencingUser = Get-CsOnlineDialInConferencingUser -Identity $User -ErrorAction SilentlyContinue
+if ($CsOnlineDialInConferencingUser) {$CsOnlineDialInConferencingUser | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsOnlineDialInConferencingUser.txt";}
+$CsOnlineApplicationInstance = Get-CsOnlineApplicationInstance -Identity $User -ErrorAction SilentlyContinue;
+if ($CsOnlineApplicationInstance) {$CsOnlineApplicationInstance | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsOnlineApplicationInstance.txt";}
+$CsOnlineApplicationEndpoint = Get-CsOnlineApplicationEndpoint -Uri $User -ErrorAction SilentlyContinue;
+if ($CsOnlineApplicationEndpoint) {$CsOnlineApplicationEndpoint | Out-File -Encoding utf8 -FilePath "$PathOut\Get-CsOnlineApplicationEndpoint.txt";}
+Compress-Archive -DestinationPath $PathExportFile -Path "$PathOut\*";
+Remove-Item $PathOut -Recurse -Force -Confirm:$false;
+explorer "/select,$PathExportFile";
+<# </TeamsVoiceUserOverviewExport> #>
