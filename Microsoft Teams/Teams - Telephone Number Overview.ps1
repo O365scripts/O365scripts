9675c659efa6c6a6573452c8c733c67e3c6bb08a
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2022-10-07 19:32:29 -0400

    Update Teams - Telephone Number Overview.ps1
    Replaced deprecated TelephoneNumber command with the expected Get-CsPhoneNumberAssignment instead.

--- Microsoft Teams/Teams - Telephone Number Overview.ps1
+++ Microsoft Teams/Teams - Telephone Number Overview.ps1
@@ -2,12 +2,14 @@
 .SYNOPSIS
 Teams Voice Number Overview
 https://github.com/O365scripts/O365scripts/blob/master/Microsoft%20Teams/Teams%20-%20Telephone%20Number%20Overview.ps1
+
 .NOTES
+
 .LINK
 Reference:
-https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinetelephonenumber?view=skype-ps
-https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlineuser?view=skype-ps
-https://docs.microsoft.com/en-us/powershell/module/skype/get-csonlinevoiceuser?view=skype-ps
+https://learn.microsoft.com/powershell/module/skype/get-csonlinetelephonenumber
+https://learn.microsoft.com/powershell/module/skype/get-csonlineuser
+https://learn.microsoft.com/powershell/module/skype/get-csonlinevoiceuser
 #>
 
 <# Connect to Teams. #>
@@ -17,21 +19,23 @@ Import-Module MicrosoftTeams;
 Connect-MicrosoftTeams;
 
 <# Get numbers and stats. #>
-$ListAllNumbers = Get-CsOnlineTelephoneNumber;
+$ListAllNumbers = Get-CsPhoneNumberAssignment;
 $CountTotalNumbers = $ListAllNumbers.Count;
 $CountServiceNumbers = ($ListAllNumbers | Where {$_.InventoryType -eq "Service"}).Count;
 $CountUserNumbers = ($ListAllNumbers | Where {$_.InventoryType -eq "Subscriber"}).Count;
 $CountTollFreeNumbers = ($ListAllNumbers | Where {$_.InventoryType -eq "Subscriber"}).Count;
 
-<# Display report. #>
-Clear
-#Write-Host -NoNewLine "# of X: "; Write-Host -Fore Yellow $CountX;
+<# Display number inventory report (incomplete). #>
+Clear;
 Write-Host -NoNewLine "Total Numbers: "; Write-Host -Fore Yellow $CountTotalNumbers;
 Write-Host -NoNewLine "# of User Numbers: "; Write-Host -Fore Yellow $CountUserNumbers;
 Write-Host -NoNewLine "# of Service Numbers: "; Write-Host -Fore Yellow $CountServiceNumbers;
+Write-Host -NoNewLine "  Toll: "; Write-Host -Fore Yellow ($CountServiceNumbers - $CountTollFreeNumbers);
+Write-Host -NoNewLine "  Toll Free: "; Write-Host -Fore Yellow $CountTollFreeNumbers;
 
-$ListAllNumbers | sort InventoryType,CityCode | ft -AutoSize O365Region,InventoryType,Id,CityCode,ActivationState,FriendlyName,TargetType
-$ListNumbers = $ListAllNumbers | sort InventoryType,CityCode | Out-GridView -PassThru -Title "Select one or multiple numbers (hold control).";
-$ListNumbers | % {Get-CsOnlineTelephoneNumber -TelephoneNumber $_.Id;
-};
-#$ListAllNumbers | select -First 1
+<# Display details of all numbers by type. #>
+$ListAllNumbers | Sort InventoryType,CityCode | Format-Table -AutoSize O365Region,InventoryType,Id,CityCode,ActivationState,FriendlyName,TargetType
+
+# Display the properties of specific numbers.
+$ListNumbers = $ListAllNumbers | Sort InventoryType,CityCode | Out-GridView -PassThru -Title "Select one or multiple numbers (hold control).";
+$ListNumbers | Format-List;
