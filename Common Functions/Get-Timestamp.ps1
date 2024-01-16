a6d1bbf3b7e090bcc92ef993044631d29da84d8f
O365 Scripts <61516533+O365scripts@users.noreply.github.com>
2024-01-15 19:19:46 -0500

    Update Get-Timestamp.ps1

--- Common Functions/Get-Timestamp.ps1
+++ Common Functions/Get-Timestamp.ps1
@@ -7,12 +7,12 @@ Version Update: 2020-12-13
 Author: https://github.com/O365scripts
 
 .LINK
-https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7
+https://learn.microsoft.com/powershell/module/microsoft.powershell.utility/get-date
 
 .EXAMPLE
-$Report | Export-Csv -Encoding utf8 -Path "Report_$(Get-Timestamp).csv";
-$Report | Out-File -Encoding utf8 -FilePath "Report_$(Get-Timestamp).txt";
+$Report | Export-Csv -Encoding utf8 -Path "Report_$(Get-Timestamp).csv"
+$Report | Out-File -Encoding utf8 -FilePath "Report_$(Get-Timestamp).txt"
 #>
 
-<# Timestamper. #>
+# Timestamper.
 function Get-Timestamp {Get-Date -Format "yyyyMMddHHmmss"}
